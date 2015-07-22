5209679aee78cb7fe984863325dc2dc12d3a64d0
Konstantin Ryabitsev <mricon@kernel.org>
2015-07-22 17:50:45 -0400

    Mark 4.0 as EOL

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,7 +25,7 @@ sys.path.append('./')
 from plugins import releases
 
 LONGTERM_KERNELS = ('3.18', '3.14', '3.12', '3.10', '3.4', '3.2', '2.6.34', '2.6.32')
-EOL_KERNELS = ('2.6.34', '3.0', '3.7', '3.8', '3.9', '3.11', '3.13', '3.15', '3.16', '3.17', '3.19')
+EOL_KERNELS = ('2.6.34', '3.0', '3.7', '3.8', '3.9', '3.11', '3.13', '3.15', '3.16', '3.17', '3.19', '4.0')
 
 GIT_MAINLINE = '/mnt/git-repos/repos/pub/scm/linux/kernel/git/torvalds/linux.git'
 GIT_STABLE   = '/mnt/git-repos/repos/pub/scm/linux/kernel/git/stable/linux-stable.git'
