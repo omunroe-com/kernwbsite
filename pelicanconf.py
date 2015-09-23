2c505c251889e3846bcd062eb930cabc89853358
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2015-09-23 16:14:38 -0700

    4.1 is a longterm kernel.
    I'll be maintaining 4.1 for the next two years, proving that after a
    decade of doing stable kernels, I still do not know any better.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,7 +24,7 @@ import sys
 sys.path.append('./')
 from plugins import releases
 
-LONGTERM_KERNELS = ('3.18', '3.14', '3.12', '3.10', '3.4', '3.2', '2.6.34', '2.6.32')
+LONGTERM_KERNELS = ('4.1', '3.18', '3.14', '3.12', '3.10', '3.4', '3.2', '2.6.34', '2.6.32')
 EOL_KERNELS = ('2.6.34', '3.0', '3.7', '3.8', '3.9', '3.11', '3.13', '3.15', '3.16', '3.17', '3.19', '4.0')
 
 GIT_MAINLINE = '/mnt/git-repos/repos/pub/scm/linux/kernel/git/torvalds/linux.git'
